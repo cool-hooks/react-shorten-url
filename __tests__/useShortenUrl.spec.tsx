d7c147242d81c5b46dc9ccba1631b1d14780703e
Jakub Biesiada <biesiadajakub@icloud.com>
2020-07-09 18:29:55 +0200

    test: update tests

--- __tests__/useShortenUrl.spec.tsx
+++ __tests__/useShortenUrl.spec.tsx
@@ -1,22 +1,38 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { renderHook } from '@testing-library/react-hooks';
+import { BitlyConfig } from 'bitly/dist/types';
 
 import { ShortenUrlProvider, useShortenUrl } from '../src';
 
+jest.mock('bitly', () => ({
+  BitlyClient: (accessToken: string, options: BitlyConfig) => ({
+    shorten: () => 'https://bit.ly/2BN8vLY',
+  }),
+}));
+
 describe('useShortenUrl', () => {
-  it('should ', () => {
-    const Component: React.FC = () => {
-      const { loading, error, data } = useShortenUrl('http://example.com/');
+  it('should return shorten URL', async () => {
+    const makeWrapper = (config: any): React.FC => ({
+      children,
+    }: {
+      children?: React.ReactNode;
+    }) => <ShortenUrlProvider config={config}>{children}</ShortenUrlProvider>;
 
-      return <p data-testid="result">{}</p>;
-    };
-
-    const { container } = render(
-      <ShortenUrlProvider config={{ accessToken: '' }}>
-        <Component />
-      </ShortenUrlProvider>
+    const { result, waitForNextUpdate } = renderHook(
+      () => useShortenUrl('http://example.com/'),
+      {
+        wrapper: makeWrapper({ accessToken: '' }),
+      }
     );
 
-    expect(container.firstChild!.textContent!).toBe();
+    expect(result.current.loading).toBe(true);
+    expect(result.current.error).toBe(null);
+    expect(result.current.data).toBe(undefined);
+
+    await waitForNextUpdate();
+
+    expect(result.current.loading).toBe(false);
+    expect(result.current.error).toBe(null);
+    expect(result.current.data).toBe('https://bit.ly/2BN8vLY');
   });
 });
