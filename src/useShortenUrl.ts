ca651f3fc2274b214f57f60df1d72dc42ee1177e
Jakub Biesiada <biesiadajakub@icloud.com>
2020-09-19 16:32:25 +0200

    refactor: replace useState with useGroupState

--- src/useShortenUrl.ts
+++ src/useShortenUrl.ts
@@ -1,32 +1,51 @@
-import { useState, useEffect, useContext } from 'react';
+import { useEffect, useContext } from 'react';
+import { useGroupState } from 'react-group-state';
 import { BitlyLink } from 'bitly/dist/types';
 
 import { ShortenUrlContext } from './context';
 
-export const useShortenUrl = (url: string) => {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<Error | null>(null);
-  const [data, setData] = useState<BitlyLink>();
+interface State {
+  readonly loading: boolean;
+  readonly error: Error | null;
+  readonly data?: BitlyLink;
+}
 
-  const { bitly } = useContext(ShortenUrlContext);
+export const useShortenUrl = (url: string) => {
+  const context = useContext(ShortenUrlContext);
+
+  if (context === undefined) {
+    throw new Error('ShortenUrlContext is not defined');
+  }
+
+  const { bitly } = context;
+
+  const [state, setState] = useGroupState<State>({
+    loading: false,
+    error: null,
+    data: undefined,
+  });
 
   useEffect(() => {
     const shorten = async () => {
-      setLoading(true);
+      setState({ loading: true });
 
       try {
-        setData(await bitly.shorten(url));
-
-        setLoading(false);
+        setState({
+          data: await bitly.shorten(url),
+          loading: false,
+        });
       } catch (err) {
-        setError(err);
-
-        setLoading(false);
+        setState({
+          error: err,
+          loading: false,
+        });
       }
     };
 
     shorten();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [bitly, url]);
 
-  return { loading, error, data };
+  return state;
 };
