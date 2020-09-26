1c740d13479a8cb8fdcdeed4bbedf401c06ce7e3
Jakub Biesiada <biesiadajakub@icloud.com>
2020-09-27 00:16:29 +0200

    chore: update types

--- src/context.tsx
+++ src/context.tsx
@@ -2,21 +2,25 @@ import React from 'react';
 import { BitlyClient } from 'bitly';
 import { BitlyConfig } from 'bitly/dist/types';
 
-interface Config {
-  accessToken: string;
-  options?: BitlyConfig;
+interface ContextProps {
+  readonly bitly: BitlyClient;
 }
 
-export const ShortenUrlContext = React.createContext<{
-  bitly: BitlyClient;
-}>({
+export const ShortenUrlContext = React.createContext<ContextProps>({
   bitly: {} as BitlyClient,
 });
 
-export const ShortenUrlProvider: React.FC<{ config: Config }> = ({
-  children,
-  config,
-}) => {
+interface Config {
+  readonly accessToken: string;
+  readonly options?: BitlyConfig;
+}
+
+interface ProviderProps {
+  readonly children: React.ReactNode;
+  readonly config: Config;
+}
+
+export const ShortenUrlProvider = ({ children, config }: ProviderProps) => {
   const { accessToken, options = {} } = config;
 
   const bitly = new BitlyClient(accessToken, options);
