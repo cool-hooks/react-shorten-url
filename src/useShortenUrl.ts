85eab71c76e41597204bf85d0c5fe837492d5b01
Jakub Biesiada <biesiadajakub@icloud.com>
2021-06-21 23:09:08 +0200

    chore: update

--- src/useShortenUrl.ts
+++ src/useShortenUrl.ts
@@ -4,12 +4,12 @@ import { BitlyLink } from 'bitly/dist/types';
 
 import { ShortenUrlContext } from './context';
 
-export const useShortenUrl = (url: string, {}: any) => {
+export const useShortenUrl = (url: string) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<Error | null>(null);
   const [data, setData] = useState<BitlyLink>();
 
-  const { bitly } = useSafeContext(ShortenUrlContext);
+  const bitly = useSafeContext(ShortenUrlContext);
 
   useEffect(() => {
     const shorten = async () => {
